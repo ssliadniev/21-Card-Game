600250dd3bf36df02b774118fdaea782f875fbf1
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-10-14 23:40:51 +0300

    feature/G21/Cards: fix Cards and Deck classes

--- cards.py
+++ cards.py
@@ -8,57 +8,43 @@ class Cards:
     """A card object, which have a suit and rank."""
 
     SUITS = ("spades", "hearts", "diamonds", "clubs")
-    RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"]
+    RANKS = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
 
     def __init__(self, rank, suit, points):
-        self.rank = rank
-        self.suit = suit
-        self.points = points
-        self.image = Card(rank, suit[0])
-
-    def get_card_image(self):
-        return self.image
+        self.card = Card(rank, suit, value=points, hidden=False, picture=True)
 
     def __str__(self):
-        return f"{self.rank} of {self.suit}"
+        return f"{self.card}"
 
 
 class Deck(object):
     """A deck containing 52 cards."""
 
     def __init__(self):
-        self.cards = self._generate_deck()
-        shuffle(self.cards)
+        self.deck = self.generate_deck()
+        shuffle(self.deck)
 
-    def _generate_deck(self):
-        points: int
+    @staticmethod
+    def generate_deck():
+        points: int = 0
         cards: list = []
         for suit, rank in product(Cards.SUITS, Cards.RANKS):
-            if rank == "Ace":
+            if rank == "A":
                 points = 11
-            elif rank == "King":
+            elif rank == "K":
                 points = 4
-            elif rank == "Queen":
+            elif rank == "Q":
                 points = 3
-            elif rank == "Jack":
+            elif rank == "J":
                 points = 2
             elif rank.isdigit():
                 points = int(rank)
-            card = Card(rank, suit, points)
+            card = Cards(rank, suit, points)
             cards.append(card)
         return cards
 
-    def get_card(self):
-        if len(self) == 0:
-            return None
-        else:
-            return self.cards.pop()
+    def get_cards(self):
+        return [self.deck.pop() for _ in range(2)]
 
     def __len__(self):
-        return len(self.cards)
-
-    def __str__(self):
-        self.result = ''
-        for c in self.cards:
-            self.result = self.result + str(c) + '\n'
-        return self.result
+        return len(self.deck)
