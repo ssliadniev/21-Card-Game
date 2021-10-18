4c29180d533cf9c15882e063fc59bf9bbf1398db
Serhii Sliadniev <s.sliadniev@quantumobile.com>
2021-10-18 23:11:37 +0300

    16579-G21: remove deck attribute deck/add name attribute and get_name method

--- player.py
+++ player.py
@@ -5,25 +5,34 @@ from cards import Cards
 class Player:
     """This class represents a player in a 21 game."""
 
-    def __init__(self, isDealer, deck):
+    def __init__(self, isDealer):
         self.cards: list = []
         self.isDealer: bool = isDealer
-        self.deck = deck
+        self.name = self.get_name()
         self.score: int = 0
 
     def hit(self) -> bool:
-        self.cards.extend(self.deck.get_card(1))
+        deck = Cards()
+        self.cards.extend(deck.get_card(1))
         self.get_points_of_cards()
         if self.score > 21:
             return True
         return False
 
     def deal(self) -> bool:
-        self.cards.extend(self.deck.get_card(2))
+        deck = Cards()
+        self.cards.extend(deck.get_card(2))
         self.get_points_of_cards()
         if self.score == 21:
             return True
         return False
+
+    def get_name(self):
+        if self.isDealer:
+            self.name = input("Your name: ")
+        else:
+            self.name = "Dealer"
+        return self.name
 
     def get_points_of_cards(self) -> int:
         self.score = sum([card.value for card in self.cards])
